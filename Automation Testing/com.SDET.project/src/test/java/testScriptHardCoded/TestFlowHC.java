cb4b44bbd5776090c030b4e033c590e6ece34727
sudhakarsdk98 <sudhakarsdk98@gmail.com>
2024-03-02 21:25:04 +0530

    New Hard coded test script added

--- Automation Testing/com.SDET.project/src/test/java/testScriptHardCoded/TestFlowHC.java
+++ Automation Testing/com.SDET.project/src/test/java/testScriptHardCoded/TestFlowHC.java
@@ -1,9 +1,16 @@
 package testScriptHardCoded;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestFlowHC {
 
 	public static void main(String[] args) {
 		
+		
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("www.google.com");
 
 	}
 
